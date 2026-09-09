8670559d0e80ae8896088b6927fbbc36ca220062
Krishan Bhargav <krishanbhargav04@gmail.com>
2026-09-09 15:37:47 +0530

    Format table status history DTO to match project coding style

--- Application/Dtos/TableStatusHistories/CreateUpdateTableStatusHistoryDto.cs
+++ Application/Dtos/TableStatusHistories/CreateUpdateTableStatusHistoryDto.cs
@@ -1,15 +1,20 @@
 using System.ComponentModel.DataAnnotations;
 using Domain.Entities.Enums;
 
-namespace Application.Dtos.TableStatusHistories;
-
-public class CreateUpdateTableStatusHistoryDto
+namespace Application.Dtos.TableStatusHistories
 {
-    [Range(1, int.MaxValue)]
-    public int TableId { get; set; }
-    [Required]
-    public TableStatus Status { get; set; }
-    public DateTime StartTime { get; set; }
-    public DateTime? EndTime { get; set; }
-    public int? CreatedByUserId { get; set; }
+    public class CreateUpdateTableStatusHistoryDto
+    {
+        [Range(1, int.MaxValue)]
+        public int TableId { get; set; }
+
+        [Required]
+        public TableStatus Status { get; set; }
+
+        public DateTime? StartTime { get; set; }
+
+        public DateTime? EndTime { get; set; }
+
+        public int? CreatedByUserId { get; set; }
+    }
 }
