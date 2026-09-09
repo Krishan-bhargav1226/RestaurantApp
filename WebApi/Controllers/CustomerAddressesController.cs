a3e0fcb3cf867b8970def3b14a31177c6d71b80c
Krishan Bhargav <krishanbhargav04@gmail.com>
2026-09-09 15:36:57 +0530

    Rewrite customer address controller in project coding style

--- WebApi/Controllers/CustomerAddressesController.cs
+++ WebApi/Controllers/CustomerAddressesController.cs
@@ -2,17 +2,60 @@ using Application.Applications.CustomerAddresses;
 using Application.Dtos.CustomerAddresses;
 using Microsoft.AspNetCore.Mvc;
 
-namespace WebApi.Controllers;
-
-[ApiController]
-[Route("api/[controller]")]
-public class CustomerAddressesController : ControllerBase
+namespace WebApi.Controllers
 {
-    private readonly ICustomerAddressApplication _application;
-    public CustomerAddressesController(ICustomerAddressApplication application) => _application = application;
-    [HttpGet("customer/{customerId:int}")] public async Task<IActionResult> GetByCustomer(int customerId) => Ok(await _application.GetByCustomerIdAsync(customerId));
-    [HttpGet("{id:int}")] public async Task<IActionResult> GetById(int id) => Ok(await _application.GetByIdAsync(id));
-    [HttpPost] public async Task<IActionResult> Create(CreateUpdateCustomerAddressDto input) => Ok(await _application.CreateAsync(input));
-    [HttpPut("{id:int}")] public async Task<IActionResult> Update(int id, CreateUpdateCustomerAddressDto input) => Ok(await _application.UpdateAsync(id, input));
-    [HttpDelete("{id:int}")] public async Task<IActionResult> Delete(int id) { await _application.DeleteAsync(id); return NoContent(); }
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CustomerAddressesController : ControllerBase
+    {
+        private readonly ICustomerAddressApplication _customerAddressApplication;
+
+        public CustomerAddressesController(
+            ICustomerAddressApplication customerAddressApplication)
+        {
+            _customerAddressApplication = customerAddressApplication;
+        }
+
+        [HttpGet("customer/{customerId:int}")]
+        public async Task<IActionResult> GetByCustomer(int customerId)
+        {
+            var result = await _customerAddressApplication.GetByCustomerIdAsync(customerId);
+
+            return Ok(result);
+        }
+
+        [HttpGet("{id:int}")]
+        public async Task<IActionResult> GetById(int id)
+        {
+            var result = await _customerAddressApplication.GetByIdAsync(id);
+
+            return Ok(result);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(CreateUpdateCustomerAddressDto input)
+        {
+            var result = await _customerAddressApplication.CreateAsync(input);
+
+            return Ok(result);
+        }
+
+        [HttpPut("{id:int}")]
+        public async Task<IActionResult> Update(
+            int id,
+            [FromBody] CreateUpdateCustomerAddressDto input)
+        {
+            var result = await _customerAddressApplication.UpdateAsync(id, input);
+
+            return Ok(result);
+        }
+
+        [HttpDelete("{id:int}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            await _customerAddressApplication.DeleteAsync(id);
+
+            return NoContent();
+        }
+    }
 }
