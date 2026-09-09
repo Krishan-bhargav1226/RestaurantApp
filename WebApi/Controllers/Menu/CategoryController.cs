fcb1c66cbe74da2ef9e34583322c8e553cf49687
Krishan Bhargav <krishanbhargav04@gmail.com>
2026-09-09 17:24:03 +0530

    Protect category APIs with management roles

--- WebApi/Controllers/Menu/CategoryController.cs
+++ WebApi/Controllers/Menu/CategoryController.cs
@@ -1,11 +1,13 @@
 ﻿using Application.Applications.Categories;
 using Application.Dtos.Categories;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace WebApi.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
+    [Authorize(Roles = "SuperAdmin,BranchAdmin,Manager")]
     public class CategoryController : ControllerBase
     {
         private readonly ICategoryApplication _categoryApplication;
@@ -19,7 +21,6 @@ namespace WebApi.Controllers
         public async Task<IActionResult> Create(CreateUpdateCategoryDto input)
         {
             var result = await _categoryApplication.CreateAsync(input);
-
             return Ok(result);
         }
 
@@ -27,7 +28,6 @@ namespace WebApi.Controllers
         public async Task<IActionResult> GetAll()
         {
             var result = await _categoryApplication.GetAllAsync();
-
             return Ok(result);
         }
 
@@ -35,17 +35,13 @@ namespace WebApi.Controllers
         public async Task<IActionResult> GetById(int id)
         {
             var result = await _categoryApplication.GetByIdAsync(id);
-
             return Ok(result);
         }
 
         [HttpPut("{id:int}")]
-        public async Task<IActionResult> Update(
-            int id,
-            [FromBody] CreateUpdateCategoryDto input)
+        public async Task<IActionResult> Update(int id, [FromBody] CreateUpdateCategoryDto input)
         {
             var result = await _categoryApplication.UpdateAsync(id, input);
-
             return Ok(result);
         }
 
@@ -53,7 +49,6 @@ namespace WebApi.Controllers
         public async Task<IActionResult> Delete(int id)
         {
             await _categoryApplication.DeleteAsync(id);
-
             return NoContent();
         }
     }
