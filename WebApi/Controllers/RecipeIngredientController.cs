9c919c5ee47d70bdd298bce40135434e756cc610
Krishan Bhargav <krishanbhargav04@gmail.com>
2026-09-09 14:39:41 +0530

    Add RecipeIngredient controller

---
+++ WebApi/Controllers/RecipeIngredientController.cs
@@ -0,0 +1,53 @@
+using Application.Applications.RecipeIngredients;
+using Application.Dtos.RecipeIngredients;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RecipeIngredientController : ControllerBase
+    {
+        private readonly IRecipeIngredientApplication _recipeIngredientApplication;
+
+        public RecipeIngredientController(IRecipeIngredientApplication recipeIngredientApplication)
+        {
+            _recipeIngredientApplication = recipeIngredientApplication;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(CreateUpdateRecipeIngredientDto input)
+        {
+            var result = await _recipeIngredientApplication.CreateAsync(input);
+            return Ok(result);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            var result = await _recipeIngredientApplication.GetAllAsync();
+            return Ok(result);
+        }
+
+        [HttpGet("{id:int}")]
+        public async Task<IActionResult> GetById(int id)
+        {
+            var result = await _recipeIngredientApplication.GetByIdAsync(id);
+            return Ok(result);
+        }
+
+        [HttpPut("{id:int}")]
+        public async Task<IActionResult> Update(int id, CreateUpdateRecipeIngredientDto input)
+        {
+            var result = await _recipeIngredientApplication.UpdateAsync(id, input);
+            return Ok(result);
+        }
+
+        [HttpDelete("{id:int}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            await _recipeIngredientApplication.DeleteAsync(id);
+            return NoContent();
+        }
+    }
+}
