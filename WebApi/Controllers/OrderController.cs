75af51a440d8159885fe7439e8a3ab6d4e28399b
Krishan Bhargav <krishanbhargav04@gmail.com>
2026-09-09 15:36:00 +0530

    Rewrite Order controller in project coding style

--- WebApi/Controllers/OrderController.cs
+++ WebApi/Controllers/OrderController.cs
@@ -2,18 +2,59 @@ using Application.Applications.Orders;
 using Application.Dtos.Orders;
 using Microsoft.AspNetCore.Mvc;
 
-namespace WebApi.Controllers;
-
-[Route("api/[controller]")]
-[ApiController]
-public class OrderController : ControllerBase
+namespace WebApi.Controllers
 {
-    private readonly IOrderApplication _application;
-    public OrderController(IOrderApplication application) => _application = application;
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OrderController : ControllerBase
+    {
+        private readonly IOrderApplication _orderApplication;
 
-    [HttpPost] public async Task<IActionResult> Create(CreateUpdateOrderDto input) => Ok(await _application.CreateAsync(input));
-    [HttpGet] public async Task<IActionResult> GetAll() => Ok(await _application.GetAllAsync());
-    [HttpGet("{id:int}")] public async Task<IActionResult> GetById(int id) => Ok(await _application.GetByIdAsync(id));
-    [HttpPut("{id:int}")] public async Task<IActionResult> Update(int id, CreateUpdateOrderDto input) => Ok(await _application.UpdateAsync(id, input));
-    [HttpDelete("{id:int}")] public async Task<IActionResult> Delete(int id) { await _application.DeleteAsync(id); return NoContent(); }
+        public OrderController(IOrderApplication orderApplication)
+        {
+            _orderApplication = orderApplication;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(CreateUpdateOrderDto input)
+        {
+            var result = await _orderApplication.CreateAsync(input);
+
+            return Ok(result);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            var result = await _orderApplication.GetAllAsync();
+
+            return Ok(result);
+        }
+
+        [HttpGet("{id:int}")]
+        public async Task<IActionResult> GetById(int id)
+        {
+            var result = await _orderApplication.GetByIdAsync(id);
+
+            return Ok(result);
+        }
+
+        [HttpPut("{id:int}")]
+        public async Task<IActionResult> Update(
+            int id,
+            [FromBody] CreateUpdateOrderDto input)
+        {
+            var result = await _orderApplication.UpdateAsync(id, input);
+
+            return Ok(result);
+        }
+
+        [HttpDelete("{id:int}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            await _orderApplication.DeleteAsync(id);
+
+            return NoContent();
+        }
+    }
 }
