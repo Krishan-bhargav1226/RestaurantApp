72687f52941329bc7bc0495daa9c981d567c2a9e
Krishan Bhargav <krishanbhargav04@gmail.com>
2026-09-09 17:23:55 +0530

    Protect branch APIs with branch administration roles

--- WebApi/Controllers/Branch/BranchController.cs
+++ WebApi/Controllers/Branch/BranchController.cs
@@ -1,5 +1,6 @@
 ﻿using Application.Applications.Branches;
 using Application.DTOs.Branches;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -7,6 +8,7 @@ namespace WebApi.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
+    [Authorize(Roles = "SuperAdmin,BranchAdmin")]
     public class BranchController : ControllerBase
     {
         private readonly IBranchApplication _branchApplication;
@@ -20,44 +22,34 @@ namespace WebApi.Controllers
         public async Task<IActionResult> Create(CreateUpdateBranchDto input)
         {
             var result = await _branchApplication.CreateAsync(input);
-
             return Ok(result);
         }
 
-        // GET: api/Branch
         [HttpGet]
         public async Task<IActionResult> GetAll()
         {
             var result = await _branchApplication.GetAllAsync();
-
             return Ok(result);
         }
-        // GET: api/Branch/1
+
         [HttpGet("{id:int}")]
         public async Task<IActionResult> GetById(int id)
         {
             var result = await _branchApplication.GetByIdAsync(id);
-
             return Ok(result);
         }
 
-        // PUT: api/Branch/1
         [HttpPut("{id:int}")]
-        public async Task<IActionResult> Update(
-            int id,
-            [FromBody] CreateUpdateBranchDto input)
+        public async Task<IActionResult> Update(int id, [FromBody] CreateUpdateBranchDto input)
         {
             var result = await _branchApplication.UpdateAsync(id, input);
-
             return Ok(result);
         }
 
-        // DELETE: api/Branch/1
         [HttpDelete("{id:int}")]
         public async Task<IActionResult> Delete(int id)
         {
             await _branchApplication.DeleteAsync(id);
-
             return NoContent();
         }
     }
