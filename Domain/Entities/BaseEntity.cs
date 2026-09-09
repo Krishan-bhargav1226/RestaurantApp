b36c6f31398875894c4d59a2625f72e472804cb1
Krishan Bhargav <krishanbhargav04@gmail.com>
2026-09-09 16:14:52 +0530

    Restore base entity after module organization

---
+++ Domain/Entities/BaseEntity.cs
@@ -0,0 +1,14 @@
+﻿namespace Domain.Entities;
+
+public  class BaseEntity
+{
+    public int Id { get; set; }
+
+    public bool IsActive { get; set; } = true;
+
+    public bool IsDeleted { get; set; } = false;
+
+    public DateTime CreatedDate { get; set; } = DateTime.UtcNow;
+
+    public DateTime? UpdatedDate { get; set; }
+}
