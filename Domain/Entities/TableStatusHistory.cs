37a41c0c05aba228b952c2faa204e55195bc3160
Krishan Bhargav <krishanbhargav04@gmail.com>
2026-09-09 15:37:26 +0530

    Format TableStatusHistory entity to match project coding style

--- Domain/Entities/TableStatusHistory.cs
+++ Domain/Entities/TableStatusHistory.cs
@@ -1,19 +1,20 @@
 using System.ComponentModel.DataAnnotations;
 using Domain.Entities.Enums;
 
-namespace Domain.Entities;
-
-public class TableStatusHistory : BaseEntity
+namespace Domain.Entities
 {
-    [Required]
-    public int TableId { get; set; }
+    public class TableStatusHistory : BaseEntity
+    {
+        [Required]
+        public int TableId { get; set; }
 
-    [Required]
-    public TableStatus Status { get; set; }
+        [Required]
+        public TableStatus Status { get; set; }
 
-    public DateTime StartTime { get; set; }
+        public DateTime StartTime { get; set; }
 
-    public DateTime? EndTime { get; set; }
+        public DateTime? EndTime { get; set; }
 
-    public int? CreatedByUserId { get; set; }
+        public int? CreatedByUserId { get; set; }
+    }
 }
