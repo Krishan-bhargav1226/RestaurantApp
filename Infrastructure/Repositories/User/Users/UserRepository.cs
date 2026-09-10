40d92e2a78738259ec59a2baafa746cbd708b8cf
Krishan Bhargav <krishanbhargav04@gmail.com>
2026-09-10 10:55:39 +0530

    Implement User repository

---
+++ Infrastructure/Repositories/User/Users/UserRepository.cs
@@ -0,0 +1,50 @@
+using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Repositories.Users;
+
+public class UserRepository : IUserRepository
+{
+    private readonly DataContext _context;
+
+    public UserRepository(DataContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<User> CreateAsync(User user)
+    {
+        _context.Users.Add(user);
+        await _context.SaveChangesAsync();
+        return user;
+    }
+
+    public async Task<List<User>> GetAllAsync()
+    {
+        return await _context.Users
+            .AsNoTracking()
+            .ToListAsync();
+    }
+
+    public async Task<User?> GetByIdAsync(int id)
+    {
+        return await _context.Users
+            .AsNoTracking()
+            .FirstOrDefaultAsync(x => x.Id == id);
+    }
+
+    public async Task<User> UpdateAsync(User user)
+    {
+        _context.Users.Update(user);
+        await _context.SaveChangesAsync();
+        return user;
+    }
+
+    public async Task DeleteAsync(User user)
+    {
+        user.IsDeleted = true;
+        user.UpdatedDate = DateTime.UtcNow;
+        _context.Users.Update(user);
+        await _context.SaveChangesAsync();
+    }
+}
