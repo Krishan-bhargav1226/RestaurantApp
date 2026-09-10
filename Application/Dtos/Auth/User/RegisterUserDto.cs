d98dc28ed1c866d730e5b2ebd0fd57c985808c84
Krishan Bhargav <krishanbhargav04@gmail.com>
2026-09-10 11:13:07 +0530

    Split auth: add user registration DTO

---
+++ Application/Dtos/Auth/User/RegisterUserDto.cs
@@ -0,0 +1,22 @@
+using System.ComponentModel.DataAnnotations;
+using Domain.Entities.Enums;
+
+namespace Application.Dtos.Auth.User;
+
+public class RegisterUserDto
+{
+    [Required]
+    [MaxLength(150)]
+    public string FullName { get; set; } = string.Empty;
+
+    [Required]
+    [EmailAddress]
+    [MaxLength(150)]
+    public string Email { get; set; } = string.Empty;
+
+    [Required]
+    [MinLength(6)]
+    public string Password { get; set; } = string.Empty;
+
+    public UserRole Role { get; set; } = UserRole.Staff;
+}
