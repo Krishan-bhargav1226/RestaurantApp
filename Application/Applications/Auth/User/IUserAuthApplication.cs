86f887688d9bdd0426001c32ba92d14c49100b8e
Krishan Bhargav <krishanbhargav04@gmail.com>
2026-09-10 11:14:25 +0530

    Split auth: add user auth application interface

---
+++ Application/Applications/Auth/User/IUserAuthApplication.cs
@@ -0,0 +1,15 @@
+using Application.Dtos.Auth.User;
+
+namespace Application.Applications.Auth.User;
+
+public interface IUserAuthApplication
+{
+    Task<UserRegistrationResponseDto> RegisterAsync(RegisterUserDto input);
+    Task<string> GenerateRegistrationOtpAsync(string email);
+    Task<UserRegistrationResponseDto> VerifyRegistrationOtpAsync(VerifyUserOtpDto input);
+    Task<UserAuthResponseDto> LoginAsync(LoginUserDto input);
+    Task<UserAuthResponseDto> RefreshTokenAsync(RefreshUserTokenDto input);
+    Task<string> ForgotPasswordAsync(string email);
+    Task ResetPasswordAsync(ResetUserPasswordDto input);
+    Task<UserRegistrationResponseDto> AssignRoleAsync(int userId, AssignRoleDto input);
+}
