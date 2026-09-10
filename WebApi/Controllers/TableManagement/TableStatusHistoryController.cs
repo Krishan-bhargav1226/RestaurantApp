7b77e6932b497010f7178f45ac8b370c24d7f0b2
Krishan Bhargav <krishanbhargav04@gmail.com>
2026-09-10 12:06:11 +0530

    Protect table status history endpoints

--- WebApi/Controllers/TableManagement/TableStatusHistoryController.cs
+++ WebApi/Controllers/TableManagement/TableStatusHistoryController.cs
@@ -1,17 +1,18 @@
 using Application.Applications.TableStatusHistories;
 using Application.Dtos.TableStatusHistories;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace WebApi.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
+    [Authorize(Roles = "SuperAdmin,BranchAdmin,Manager,Staff")]
     public class TableStatusHistoryController : ControllerBase
     {
         private readonly ITableStatusHistoryApplication _tableStatusHistoryApplication;
 
-        public TableStatusHistoryController(
-            ITableStatusHistoryApplication tableStatusHistoryApplication)
+        public TableStatusHistoryController(ITableStatusHistoryApplication tableStatusHistoryApplication)
         {
             _tableStatusHistoryApplication = tableStatusHistoryApplication;
         }
@@ -20,7 +21,6 @@ namespace WebApi.Controllers
         public async Task<IActionResult> Create(CreateUpdateTableStatusHistoryDto input)
         {
             var result = await _tableStatusHistoryApplication.CreateAsync(input);
-
             return Ok(result);
         }
 
@@ -28,7 +28,6 @@ namespace WebApi.Controllers
         public async Task<IActionResult> GetAll()
         {
             var result = await _tableStatusHistoryApplication.GetAllAsync();
-
             return Ok(result);
         }
 
@@ -36,17 +35,13 @@ namespace WebApi.Controllers
         public async Task<IActionResult> GetById(int id)
         {
             var result = await _tableStatusHistoryApplication.GetByIdAsync(id);
-
             return Ok(result);
         }
 
         [HttpPut("{id:int}")]
-        public async Task<IActionResult> Update(
-            int id,
-            [FromBody] CreateUpdateTableStatusHistoryDto input)
+        public async Task<IActionResult> Update(int id, [FromBody] CreateUpdateTableStatusHistoryDto input)
         {
             var result = await _tableStatusHistoryApplication.UpdateAsync(id, input);
-
             return Ok(result);
         }
 
@@ -54,7 +49,6 @@ namespace WebApi.Controllers
         public async Task<IActionResult> Delete(int id)
         {
             await _tableStatusHistoryApplication.DeleteAsync(id);
-
             return NoContent();
         }
     }
