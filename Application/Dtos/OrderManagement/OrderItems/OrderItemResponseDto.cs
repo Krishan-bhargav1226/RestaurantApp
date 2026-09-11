63fcb6e0a4b91c6bc10066f2e9158f8eb70cd70d
Krishan Bhargav <krishanbhargav04@gmail.com>
2026-09-11 10:00:44 +0530

    refactor(api): remove auth dependencies and finalize CRUD contracts

--- Application/Dtos/OrderManagement/OrderItems/OrderItemResponseDto.cs
+++ Application/Dtos/OrderManagement/OrderItems/OrderItemResponseDto.cs
@@ -5,7 +5,7 @@ public class OrderItemResponseDto
     public int Id { get; set; }
     public int OrderId { get; set; }
     public int ProductId { get; set; }
-    public decimal Quantity { get; set; }
+    public int Quantity { get; set; }
     public decimal UnitPrice { get; set; }
     public decimal TotalPrice { get; set; }
     public string? SpecialInstructions { get; set; }
