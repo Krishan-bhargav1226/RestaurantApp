07db0ab506804275c50aa7e054502bda7ee9d501
Krishan Bhargav <krishanbhargav04@gmail.com>
2026-09-11 10:00:34 +0530

    refactor(auth): remove obsolete authentication DTOs

--- Application/Dtos/Auth/Customer/RefreshCustomerTokenDto.cs
+++
@@ -1,9 +0,0 @@
-using System.ComponentModel.DataAnnotations;
-
-namespace Application.Dtos.Auth.Customer;
-
-public class RefreshCustomerTokenDto
-{
-    [Required]
-    public string RefreshToken { get; set; } = string.Empty;
-}
