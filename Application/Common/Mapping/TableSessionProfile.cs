5ed4358cd66241f2e76d923f7356421c7753b4fb
Krishan Bhargav <krishanbhargav04@gmail.com>
2026-09-14 16:08:20 +0530

    Add TableSession AutoMapper profile

---
+++ Application/Common/Mapping/TableSessionProfile.cs
@@ -0,0 +1,14 @@
+using Application.Dtos.TableSessions;
+using AutoMapper;
+using Domain.Entities;
+
+namespace Application.Common.Mapping;
+
+public class TableSessionProfile : Profile
+{
+    public TableSessionProfile()
+    {
+        CreateMap<CreateUpdateTableSessionDto, TableSession>();
+        CreateMap<TableSession, TableSessionResponseDto>();
+    }
+}
