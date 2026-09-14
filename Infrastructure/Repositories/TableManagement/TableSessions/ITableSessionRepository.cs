0024deb5dc8a9710cfa75ca8a2dcec56c0fe5198
Krishan Bhargav <krishanbhargav04@gmail.com>
2026-09-14 16:07:50 +0530

    Add TableSession repository interface

---
+++ Infrastructure/Repositories/TableManagement/TableSessions/ITableSessionRepository.cs
@@ -0,0 +1,12 @@
+using Domain.Entities;
+
+namespace Infrastructure.Repositories.TableSessions;
+
+public interface ITableSessionRepository
+{
+    Task<TableSession> CreateAsync(TableSession session);
+    Task<List<TableSession>> GetAllAsync();
+    Task<TableSession?> GetByIdAsync(int id);
+    Task<TableSession> UpdateAsync(TableSession session);
+    Task DeleteAsync(TableSession session);
+}
