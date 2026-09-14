2f58e7e9804a7cfcf321c0a3d20af018133e6bd0
Krishan Bhargav <krishanbhargav04@gmail.com>
2026-09-14 16:08:13 +0530

    Add TableSession application

---
+++ Application/Applications/TableManagement/TableSessions/TableSessionApplication.cs
@@ -0,0 +1,62 @@
+using Application.Dtos.TableSessions;
+using AutoMapper;
+using Domain.Entities;
+using Infrastructure.Repositories.TableSessions;
+
+namespace Application.Applications.TableSessions;
+
+public class TableSessionApplication : ITableSessionApplication
+{
+    private readonly ITableSessionRepository _tableSessionRepository;
+    private readonly IMapper _mapper;
+
+    public TableSessionApplication(ITableSessionRepository tableSessionRepository, IMapper mapper)
+    {
+        _tableSessionRepository = tableSessionRepository;
+        _mapper = mapper;
+    }
+
+    public async Task<TableSessionResponseDto> CreateAsync(CreateUpdateTableSessionDto input)
+    {
+        var session = _mapper.Map<TableSession>(input);
+        session.StartTime = input.StartTime ?? DateTime.UtcNow;
+        session.IsActive = true;
+        session.IsDeleted = false;
+        return _mapper.Map<TableSessionResponseDto>(await _tableSessionRepository.CreateAsync(session));
+    }
+
+    public async Task<List<TableSessionResponseDto>> GetAllAsync()
+    {
+        var sessions = await _tableSessionRepository.GetAllAsync();
+        return _mapper.Map<List<TableSessionResponseDto>>(sessions);
+    }
+
+    public async Task<TableSessionResponseDto?> GetByIdAsync(int id)
+    {
+        var session = await _tableSessionRepository.GetByIdAsync(id);
+        return session == null ? null : _mapper.Map<TableSessionResponseDto>(session);
+    }
+
+    public async Task<TableSessionResponseDto?> UpdateAsync(int id, CreateUpdateTableSessionDto input)
+    {
+        var session = await _tableSessionRepository.GetByIdAsync(id);
+        if (session == null)
+            return null;
+
+        _mapper.Map(input, session);
+        session.StartTime = input.StartTime ?? session.StartTime;
+        session.IsActive = input.EndTime == null;
+
+        return _mapper.Map<TableSessionResponseDto>(await _tableSessionRepository.UpdateAsync(session));
+    }
+
+    public async Task<bool> DeleteAsync(int id)
+    {
+        var session = await _tableSessionRepository.GetByIdAsync(id);
+        if (session == null)
+            return false;
+
+        await _tableSessionRepository.DeleteAsync(session);
+        return true;
+    }
+}
